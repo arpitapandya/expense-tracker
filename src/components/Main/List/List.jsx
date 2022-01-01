3da7737fa01fff6a0a43609f3b1f9d66921f6a1b
Arpita <arpya74@gmail.com>
2022-01-01 18:34:52 -0500

    created form for main component

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, ListitemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+
+const List = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default List
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import Form from './Form/Form';
+import useStyles from './styles';
+
+const Main = () => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardContent>
+                <Typography align='center' variant='h5'>Total Balance $100</Typography>
+                <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+                    Try saying: Add income for $100 in category salary for Monday...
+                </Typography>
+                <Divider />
+                {/* form */}
+                <Form />
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                {/* List */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
