301058c0ef46b66d3ed6d9fbd297c06fe6bb0cf7
Arpita <arpya74@gmail.com>
2022-01-09 15:54:53 -0500

    added hook for speech transcript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
+
 import Main from './components/Main/Main';
 import Details from './components/Details/Details';
 import useStyles from './styles';
@@ -19,8 +21,12 @@ const App = () => {
                     <Details title='Expense' />
                 </Grid>
             </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton />
+                <ErrorPanel />
+            </PushToTalkButtonContainer>
         </div>
-    )
+    );
 }
 
 export default App;
--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { TextField, Typography, Grid, Button, FormControl, Select, MenuItem, InputLabel } from '@material-ui/core'
 import { ExpenseTrackerContext } from '../../../context/context';
 import { v4 as uuidv4 } from 'uuid';
+import { useSpeechContext } from '@speechly/react-client';
 import formatDate from '../../../utils/formatDate';
 import useStyles from './styles';
 
@@ -17,6 +18,7 @@ const Form = () => {
     const classes = useStyles();
     const [formData, setFormData] = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
+    const { segment } = useSpeechContext();
 
     const createTransaction = () => {
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
