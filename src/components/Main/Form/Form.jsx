8b4c3f37030a5dc3add2ad598557e0623cfe26dd
Arpita <arpya74@gmail.com>
2022-01-09 22:37:16 -0500

    added logic to form

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -7,6 +7,7 @@ import formatDate from '../../../utils/formatDate';
 import useStyles from './styles';
 
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import CustomizedSnackbar from '../../Snackbar/Snackbar';
 
 const initialState = {
     amount: '',
@@ -19,10 +20,12 @@ const Form = () => {
     const [formData, setFormData] = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
     const { segment } = useSpeechContext();
+    const[open, setOpen] = useState(false);
 
     const createTransaction = () => {
         if(Number.isNaN(Number(formData.amount)) || formData.date.includes('-')) return;
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+        setOpen(true);
         addTransaction(transaction);
         setFormData(initialState);
     }
@@ -71,6 +74,7 @@ const Form = () => {
     // console.log(formData);
     return (
         <Grid container spacing={2}>
+            <CustomizedSnackbar open={open} setOpen={setOpen} />
             <Grid item xs={12}>
                 <Typography align='center' variant='subtitle2' gutterBottom>
                     {segment && segment.words.map((w) => w.value).join(" ")}
