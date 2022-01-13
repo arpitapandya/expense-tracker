31e24bfd6c271e124c8b2449b3faa9c8c22a1276
Arpita <arpya74@gmail.com>
2022-01-13 18:19:41 -0500

    final changes

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -4,14 +4,14 @@ const isIncome = Math.round(Math.random());
 
 const InfoCard = () => {
     return (
-        <div style={{ textAlign: 'center', padding: '0 10%' }}>
+        <div elevation={3} style={{ textAlign: 'center', padding: '0 10%' }}>
             Try saying: <br />
             Add { isIncome ? 'Income ' : 'Expense ' }
             for { isIncome ? '$100 ' : '$50 '}
             in category {isIncome ? 'Business ' : 'House ' }
             for {isIncome ? 'Monday ' : 'Tuesday ' }
         </div>
-    )
-}
+    );
+};
 
 export default InfoCard;
--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -20,15 +20,21 @@ const Form = () => {
     const [formData, setFormData] = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
     const { segment } = useSpeechContext();
-    const[open, setOpen] = useState(false);
+    const[open, setOpen] = React.useState(false);
 
     const createTransaction = () => {
-        if(Number.isNaN(Number(formData.amount)) || formData.date.includes('-')) return;
-        const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+        if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
+        if (incomeCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({ ...formData, type: 'Income' });
+        } else if (expenseCategories.map((iC) => iC.type).includes(formData.category)) {
+            setFormData({ ...formData, type: 'Expense' });
+        }
+        
+        // const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
         setOpen(true);
-        addTransaction(transaction);
+        addTransaction({ ...formData, amount: Number(formData.amount), id: uuidv4() });
         setFormData(initialState);
-    }
+    };
 
     useEffect(() => {
         if(segment) {
@@ -77,7 +83,11 @@ const Form = () => {
             <CustomizedSnackbar open={open} setOpen={setOpen} />
             <Grid item xs={12}>
                 <Typography align='center' variant='subtitle2' gutterBottom>
-                    {segment && segment.words.map((w) => w.value).join(" ")}
+                    {segment ? (
+                        <div className='segment'>
+                            {segment.words.map((w) => w.value).join(" ")}
+                        </div>
+                    ) : null}
                 </Typography>
             </Grid>
             <Grid item xs={6}>
@@ -98,14 +108,14 @@ const Form = () => {
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type='number' label='Amount' fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
+                <TextField type='number' label='Amount' value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} fullWidth/>
             </Grid>
             <Grid item xs={6}>
-                <TextField type='date' label='Date' fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
+                <TextField type='date' label='Date' value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} fullWidth />
             </Grid>
-            <Button className={classes.button} variant='outlined' color='primary' fullWidth onClick={createTransaction}>CREATE</Button>
+            <Button className={classes.button} variant='outlined' color='primary' fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
-    )
-}
+    );
+};
 
 export default Form
